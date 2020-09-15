a55a3e85d5a131c95a877c30178274dd4313d066
Putraaaaaaa <70950584+Putraaaaaaa@users.noreply.github.com>
2020-09-15 18:15:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 const urls = ["https://moon5-sic-bp.glitch.me"]
+const urls = ["https://the-moon-craft-bots--wako.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
